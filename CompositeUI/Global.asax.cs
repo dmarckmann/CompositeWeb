2d45b1daa92807a31e5544edb529acd040a1b21a
Dries <dries.marckmann@gmail.com>
2012-06-21 21:21:29 +0200

    styled it a bit to make it look like a bookshop

--- CompositeUI/Global.asax.cs
+++ CompositeUI/Global.asax.cs
@@ -17,34 +17,6 @@ namespace CompositeUI
 			CompositeWeb.CompositeUIController.RegisterComponent(new QuickBook());
 		}
 
-		protected void Session_Start(object sender, EventArgs e)
-		{
-
-		}
-
-		protected void Application_BeginRequest(object sender, EventArgs e)
-		{
-
-		}
-
-		protected void Application_AuthenticateRequest(object sender, EventArgs e)
-		{
-
-		}
-
-		protected void Application_Error(object sender, EventArgs e)
-		{
-
-		}
-
-		protected void Session_End(object sender, EventArgs e)
-		{
-
-		}
-
-		protected void Application_End(object sender, EventArgs e)
-		{
-
-		}
+	
 	}
 }
